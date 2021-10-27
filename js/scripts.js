d9a58a459d8bd6e87acc3f8d57a7aeecbafa4ac5
David Johnsonn <djohnso08@gmail.com>
2021-10-26 18:37:11 -0700

    fix bug in code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInst(prompt("Enter a number:"));
+const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
